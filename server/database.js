bfbd21a51bc9c3c87d38e2f07da5400e3bce444d
Chanhyeong Cho <chu7825@gmail.com>
2017-08-18 23:34:21 +0900

    혼란 방지를 위해 변수명 변경, 주석 추가

--- server/database.js
+++ server/database.js
@@ -2,12 +2,13 @@ var MongoClient = require('mongodb').MongoClient;
 
 // Connection URL
 var url = 'mongodb://www.sopad.ml:27017/sopad';
-var instance = null;
+// MongoClient.connect 결과로 얻은 db를 이 변수에 저장. 각종 operation에서 db를 이 값으로 사용하면 됨.
+var db = null;
 
 exports.init = function (callback) {
     MongoClient.connect(url, callback);
 };
 
 exports.getInstance = function (emtpy, mongo, mongopoll) {
-    instance = mongo;
+    db = mongo;
 };
