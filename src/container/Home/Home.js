937e009dc6175e98cb49c84fe812eaabe8dc6faa
hong <hhk2945@naver.com>
2017-09-06 15:19:10 +0900

    프로젝트 생성 창 구현 (DB update구현 필요)

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -5,18 +5,38 @@ import React from 'react'
 import styles from './Home.scss'
 import Login from 'component/Login/'
 import MainScreen from 'component/MainScreen/'
+import ProjectList  from 'component/ProjectList/'
 
 class Home extends React.Component {
 
     constructor() {
-        super()
+        super();
+        this.state = {isLogIn: true};
+
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
     }
 
+    handleLogin = () => {
+        this.setState({isLogIn : true});
+    };
+    handleLogout = () => {
+        this.setState({isLogIn:false});
+    };
+
+    renderPage() {
+        if(this.state.isLogIn) {
+            return <ProjectList />
+        }
+        else{
+            return <MainScreen />
+        }
+    }
     render() {
         return (
             <div className={styles.wrapper}>
                <Login />
-               <MainScreen />
+                {this.renderPage()}
             </div>
         )
     }
