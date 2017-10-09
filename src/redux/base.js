e1bfd24431cab720d42bd8dffd7f3ca799156862
highalps <highalps93@gmail.com>
2017-10-09 16:50:36 +0900

    [수정] 오타

--- src/redux/base.js
+++ src/redux/base.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-    aseURL: 'http://external.cocotutor.ml:3000/auth/',
+    baseURL: 'http://external.cocotutor.ml:3000/auth/',
     timeout: 5000,
 })
 
