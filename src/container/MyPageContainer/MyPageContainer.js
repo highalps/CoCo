bdad442b4a5522a1dc44b0b8aca77e5b57972437
highalps <highalps93@gmail.com>
2017-11-27 21:20:54 +0900

    Change contianer name (for resolve webpack build error)

--- src/container/MyPageContainer/MyPageContainer.js
+++ src/container/MyPageContainer/MyPageContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './MyPage.scss'
+import styles from './MyPageContainer.scss'
 import client from '../../redux/base.js'
 import { connect } from 'react-redux'
 
