7d0312cc2e936b554c799d215f03c4b734c3b357
higg <highalps93@gmail.com>
2017-11-27 22:44:05 +0900

    chat button 안보이던 오류 수정

--- src/component/ChatWrapper/ChatWrapper.js
+++ src/component/ChatWrapper/ChatWrapper.js
@@ -27,7 +27,7 @@ class ChatWrapper extends React.Component {
     }
 
     renderChatButton() {
-        if (this.props.isLogged && ``) {
+        if (this.props.isLogged && !this.props.location.includes('/editor')) {
             return (
                 <ChatButton
                     showChatList={this.props.showChatList}
