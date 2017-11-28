7a13f48c57365b60f966d4fb4f83e3194151245a
higg <highalps93@gmail.com>
2017-11-28 20:50:05 +0900

    get directory from router location

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
     componentDidMount() {
         this.props.dispatch(uiActions.closeChat())
         const payload = {
-            chatId: this.props.chatId || 8025,
+            chatId: this.props.chatId || this.props.location.pathname.split('editor/')[1],
         }
         this.props.dispatch(editorActions.getDirectory(payload))
             .then(() => this.setState({ isLoading: false }))
